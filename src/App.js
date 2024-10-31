6fefec4057e4322381bf366b13044adce513ef7b
Дима <dima@MacBook-Air-Dima.local>
2024-10-31 15:05:39 +0300

    change App.js not jsx

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
+import { createElement } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 export const App = () => {
-  // Императивный, тут мы описываем как будет работать функция App
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* Императивный, тут мы описываем как будет выводиться лого */}
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h5>{new Date().getFullYear()}</h5>{" "}
-        {/* Императивный, тут мы описываем как будет работать вывод текущего года */}
-      </header>
-    </div>
+  return createElement(
+    "div",
+    { className: "App" },
+    createElement(
+      "header",
+      { className: "App-header" },
+      createElement("img", { src: logo, className: "App-logo", alt: "logo" }),
+      createElement(
+        "p",
+        null,
+        "Edit ",
+        createElement("code", null, "src/App.js"),
+        " and save to reload."
+      ),
+      createElement(
+        "a",
+        {
+          className: "App-link",
+          href: "https://reactjs.org",
+          target: "_blank",
+          rel: "noopener noreferrer",
+        },
+        "Learn React"
+      ),
+      createElement("h5", null, new Date().getFullYear())
+    )
   );
 };
